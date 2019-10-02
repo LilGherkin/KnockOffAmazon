03d14f6fb2d858f7896b1e2e40486ff191118dfd
Mark Hartman <mthartman77@gmail.com>
2019-10-02 01:01:58 -0500

    Made it so you can make multiple purchases in one running of the javascript.

--- CustomerInterface.js
+++ CustomerInterface.js
@@ -4,78 +4,114 @@ var connection = mysql.createConnection({
     host : "localhost",
     port: 3306,
     user: "root",
-    password: "This isn't my password, you'd want to use your own. Github repo.",
+    password: "#1tWothRee",
     database: "bamazon"
 });
 
-connection.connect((err) => {
-    if (err) throw err;
 
+//Flag to see if we've run the Connect function yet. 
+
+//Connect us to our server
+function Connect(){
+    connection.connect((err) => {
+        if (err) throw err;
+        //If it didn't error out then return all rows in our table
+        Actions();
+    });
+};
+
+function CheckInventory(){
     connection.query("SELECT * FROM products", (err, res) => {
         if (err) throw err;
+        //console.table formats it as a table. Ain't that neat. 
         console.table(res);
-        purchasePrompt();
+        Actions();
     });
+};
 
-});
+function Actions() {
+    inquirer.prompt([
+        {
+            name: "Request",
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["Check Inventory", "Make A Purchase", "Exit"]
+        }
+    ])
+    .then(answers => {
+        if (answers.Request === "Check Inventory"){
+            CheckInventory();
+        } else if (answers.Request === "Make A Purchase") {
+            CheckInventory2();
+        } else {
+            console.log("Thank you. Come again.");
+            connection.end();
+        }       
+    });
+};
+
+function CheckInventory2(){
+    connection.query("SELECT * FROM products", (err, res) => {
+        if (err) throw err;
+        //console.table formats it as a table. Ain't that neat. 
+        console.table(res);
+        PurchaseMaker();
+    });
+};
 
 
-function purchasePrompt() {
+function PurchaseMaker() {
     inquirer.prompt([
         {
             name: "id",
-            type: "input",
-            message: "What is the id of the item you would like to buy?",
-            validate: (value) => {
-                return !isNaN(value);
-            }
+            type: "number",
+            message: "Enter the ID of the item you'd wish to purchase."  
         }, {
             name: "units",
-            type: "input",
-            message: "How mmuch of this product would you like to buy?",
-            validate: (value) => {
-                return !isNaN(value);
-            }
+            type: "number",
+            message: "How many would you like to purchase?"
         }
-    ]).then((answer) => {
-
+    ]).then(function(answer){
         connection.query(
+            //Utilizing template literals to grab data from our table.
             `SELECT * FROM products WHERE ITEM_id=${answer.id}`,
-            (err, res) => {
+            function(err, res){
                 if (err) throw err;
-
+                //Console table is fantastic.
                 console.table(res);
-
+                //res[0] because it is returned as an array of objects and we need the first item in the array. Check to see if there's enough.
                 if (answer.units <= res[0].STOCK_QUANTITY) {
-
-                    var maths = res[0].STOCK_QUANTITY - answer.units;
+                    //Set an amount to reduce by.
+                    var ReduceQuantity = res[0].STOCK_QUANTITY - answer.units;
+                    //How much will it all cost?
                     var PriceCalculation = res[0].CUSTOMER_COST * answer.units;
+                    //Send a request to the connection to update products set
                     connection.query(
                         `UPDATE products SET ? WHERE ?`,
                         [
                             {
-                                STOCK_QUANTITY: maths
+                                STOCK_QUANTITY: ReduceQuantity
                             }, {
                                 ITEM_ID: answer.id
                             }
                         ],
                         (err) => {
                             if (err) throw err;
-
-                            console.log(`Purchasing ${answer.units} units of ${res[0].PRODUCT_NAME} for $${PriceCalculation}! Thank you for your purchase!`);
-
+                            //Message to return when there is now error. Returns the amount of an item purchased 
+                            console.log(`Purchasing ${answer.units} units of ${res[0].PRODUCT_NAME} for $${PriceCalculation}! Thank you for your purchase!`) /n;
+                            //Run Actions again to see if they want to do anything else.
+                            Actions();
+                            
                         }
                     );
+                //There wasn't enough. 
                 } else {
                     connection.end();
-                    return console.log(`Insufficient quantity!`);
-                    
+                    return console.log(`Sorry, we can't fulfill that order at that quantity!`);                  
                 }
-
-                connection.end();
-
             }
         );
-
     });
-}
+};
+//Initializes the program. 
+Connect();
